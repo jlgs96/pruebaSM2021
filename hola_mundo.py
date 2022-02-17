7373b3fd8e000ae54b90cd4a80c8c702cb029168
jlgs96 <joselgs96@gmail.com>
2022-02-17 16:59:33 +0100

    Error controlado 2

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,5 +1,10 @@
 def hola_mundo():
-    print("Hola Mundo!!!")
-def hola_mundo2():
-    print("Hola Mundo2!!!!")
+
+    print("HOLA")
+ def hola_mundo():
+
+    print("Hola al mundo")
+ def hola_mundo2():
+
+    print("Hola Mundo2!")
 hola_mundo()
